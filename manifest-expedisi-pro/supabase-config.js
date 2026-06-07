48b720e7b16481adc7b13b4e89da4ef1bf6d5b9a
AjiPutra <ajiputratech001@gmail.com>
2026-06-08 01:31:04 +0700

    Delete manifest-expedisi-pro directory

--- manifest-expedisi-pro/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const supabaseUrl = 'https://xohldcmeialjpbqyjkqh.supabase.co';
-const supabaseKey = 'sb_publishable_uuH2LT12UxSJQUPEdpEKSg_muIrzZ8a';
-
-// Use the global supabase object from UMD
-window.supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
