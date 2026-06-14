d1af4b65c5d0f6be7b6ea3cebf8e2fdd1617460e
AjiPutra <ajiputratech001@gmail.com>
2026-06-15 01:17:59 +0700

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,9 +0,0 @@
-// ============================================
-// SUPABASE CONFIG
-// Gunakan CDN UMD - tidak perlu import
-// ============================================
-const SUPABASE_URL = 'https://xohldcmeialjpbqyjkqh.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_uuH2LT12UxSJQUPEdpEKSg_muIrzZ8a';
-
-// Buat client global, siap dipakai oleh app.js
-window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
